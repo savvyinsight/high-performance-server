a189a0862a8002db8eefe2d99256ef4679df2b57
Richie <ecedwdpf@outlook.com>
2025-10-31 20:03:55 +0800

    update

---
+++ src/simExample/ThreadPollExample.cpp
@@ -0,0 +1,40 @@
+// ThreadPoll Example
+#include <condition_variable>
+#include <cstddef>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+class ThreadPool {
+    std::vector<std::thread> workers;
+    std::queue<int> connections;
+    std::condition_variable condition;
+    std::mutex queue_mutex;
+
+    bool stop = false;
+public:
+    ThreadPool(size_t threads){
+        for (int i = 0; i<threads; ++i) {
+            workers.emplace_back([this]{
+                while(true){
+                    int conn_fd;
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        this->condition.wait(lock,[this]{
+                            return this->stop || !this->connections.empty();
+                        });
+                        if(this->stop && this->connections.empty()) return;
+                        conn_fd = this->connections.front();
+                        this->connections.pop();
+                    }
+                    handle_client(conn_fd);
+                }
+            });
+        }
+    }
+    void enqueue(int conn_fd){
+        
+    }
+    ~ThreadPool(){}
+
+};
