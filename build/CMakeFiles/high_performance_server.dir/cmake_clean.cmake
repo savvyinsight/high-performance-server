f2225296962793ecdfeea5041db25eb23f3e958a
savvyinsight <ecedwdpf@outlook.com>
2025-11-26 10:46:12 +0800

    add Logger

---
+++ build/CMakeFiles/high_performance_server.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/high_performance_server.dir/src/Logger.cpp.o"
+  "CMakeFiles/high_performance_server.dir/src/Logger.cpp.o.d"
+  "CMakeFiles/high_performance_server.dir/src/ThreadPool.cpp.o"
+  "CMakeFiles/high_performance_server.dir/src/ThreadPool.cpp.o.d"
+  "CMakeFiles/high_performance_server.dir/src/main.cpp.o"
+  "CMakeFiles/high_performance_server.dir/src/main.cpp.o.d"
+  "CMakeFiles/high_performance_server.dir/src/timer_manager.cpp.o"
+  "CMakeFiles/high_performance_server.dir/src/timer_manager.cpp.o.d"
+  "high_performance_server"
+  "high_performance_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/high_performance_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
